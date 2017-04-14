fb3c2a5910efc51d191652bac2c90538f6e49921
OleksandrPavlyshch <lokdawn80@gmail.com>
2017-04-14 09:32:06 +0300

    add fonts and header

---
+++ source/elements/header/header.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(function () {
+
+	var $body = $('body')
+		, menuButtonClass = '.toggle-menu-button'
+		, menuShowClass = 'is-menu-show'
+		, scrollClass = "is-scroll";
+
+	
+	var scrollWatch = function(){
+
+		if(window.pageYOffset > window.innerHeight/6){
+			$body.addClass(scrollClass);
+		}
+
+		window.addEventListener('scroll', function () {
+			var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+			if(scrolled > window.innerHeight/6){
+				$body.addClass(scrollClass);
+				return;
+			}
+			$body.removeClass(scrollClass);
+		});
+
+	};
+
+	//init
+	scrollWatch();
+
+
+	$body.on('click', menuButtonClass,  function() {
+		$body.toggleClass(menuShowClass);
+	});
+	
+})();
