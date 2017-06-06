53aa8571ab99339fadfe55299acdd88085f96b99
OleksandrPavlyshch <lokdawn80@gmail.com>
2017-06-06 11:45:37 +0300

    update images

--- build/js/index.js
+++ build/js/index.js
@@ -1,4 +1,32 @@
 'use strict';
+
+(function () {
+
+	var $body = $('body')
+		, scrollClass = "is-scroll";
+
+	
+	var scrollWatch = function(){
+
+		if(window.pageYOffset > window.innerHeight/6){
+			$body.addClass(scrollClass);
+		}
+
+		window.addEventListener('scroll', function () {
+			var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+			if(scrolled > 30){
+				$body.addClass(scrollClass);
+				return;
+			}
+			$body.removeClass(scrollClass);
+		});
+
+	};
+
+	//init
+	scrollWatch();
+})();
+'use strict';
 (function () {
 	var $body = $('body')
 	, $menuButton = $('.toggle-menu-button')
@@ -280,34 +308,6 @@ if (!Array.prototype.filter){
 	}
 
 })(jQuery);
-'use strict';
-
-(function () {
-
-	var $body = $('body')
-		, scrollClass = "is-scroll";
-
-	
-	var scrollWatch = function(){
-
-		if(window.pageYOffset > window.innerHeight/6){
-			$body.addClass(scrollClass);
-		}
-
-		window.addEventListener('scroll', function () {
-			var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-			if(scrolled > 30){
-				$body.addClass(scrollClass);
-				return;
-			}
-			$body.removeClass(scrollClass);
-		});
-
-	};
-
-	//init
-	scrollWatch();
-})();
 'use strict';
 (function(){
 	$(document).ready(function(){
